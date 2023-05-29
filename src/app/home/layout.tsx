c80e5354eec36556c4803becb37b6657f170ea60
seulki-lee-influential <seulki.lee@influential.co.kr>
2023-05-29 23:37:00 +0900

    기본 셋

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import MainHeader from '@/components/layout/MainHeader';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MainHeader
+          title=""
+          isShow={true}
+          showBackButton={false}
+        />
+        {children}
+      </body>
     </html>
   )
 }
